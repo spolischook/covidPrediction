5f9eb2c9c6a3325c90b916c50208192de7008830
Serhii Polishchuk <spolischook@gmail.com>
2021-05-29 12:06:26 +0300

    Added gin api

--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module github.com/spolischook/covidPrediction
 go 1.16
 
 require (
-	github.com/DzananGanic/numericalgo v0.0.0-20170804125527-2b389385baf0 // indirect
+	github.com/DzananGanic/numericalgo v0.0.0-20170804125527-2b389385baf0
+	github.com/gin-gonic/gin v1.7.2
+	github.com/stretchr/testify v1.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,24 +2,35 @@ package main
 
 import (
 	"fmt"
-	"github.com/DzananGanic/numericalgo"
-	"github.com/DzananGanic/numericalgo/fit/linear"
+	"net/http"
 	"strconv"
 	"time"
 
+	"github.com/DzananGanic/numericalgo"
+	"github.com/DzananGanic/numericalgo/fit/linear"
+	"github.com/gin-gonic/gin"
 	"github.com/spolischook/covidPrediction/stats"
 )
 
 const statFile = "/home/spolischook/go/study/covid/owid-covid-data.csv"
 
 func main() {
+	router := gin.Default()
+	router.GET("/covid-predict/:countryCode", handle)
+	router.Run(":8080")
+}
+
+func handle(c *gin.Context) {
 	iter := stats.NewIterator(statFile)
 	defer iter.Close()
 
-	predict(iter, "UKR", "2021-09-01")
+	countryCode := c.Param("countryCode")
+	watchingDateStr := c.DefaultQuery("date", time.Now().Format("2006-01-02"))
+	expectedCases := predict(iter, countryCode, watchingDateStr)
+	c.String(http.StatusOK, "Hello %d", int(expectedCases))
 }
 
-func predict(iter *stats.CovidStatsIter, country string, watchingDateStr string) {
+func predict(iter *stats.CovidStatsIter, country string, watchingDateStr string) float64 {
 	var startDate *time.Time
 	var x numericalgo.Vector
 	var y numericalgo.Vector
@@ -46,8 +57,10 @@ func predict(iter *stats.CovidStatsIter, country string, watchingDateStr string)
 	expectedNewCases := lf.Predict(judgmentDay)
 
 	fmt.Printf(
-		"Expected new cases at %s(%d day): %d",
+		"Expected new cases at %s(%d day): %f\n",
 		watchingDateStr,
 		int(judgmentDay),
-		int(expectedNewCases))
+		expectedNewCases)
+
+	return expectedNewCases
 }
